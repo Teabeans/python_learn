c7ddbe6f8fe6385d0d49ffcf5de46d4946e349dc
Tim Lum <twhlum@gmail.com>
2018-03-27 16:43:40 -0700

    Update 001_CommandLineInterface.py

--- 001_CommandLineInterface.py
+++ 001_CommandLineInterface.py
@@ -2,3 +2,31 @@
 #   Module Imports
 #   https://docs.python.org/3/py-modindex.html
 #---|---|---|---|------------------------------------------------------|------|
+
+# argparse - For parsing command line arguments at the time of program execution
+
+# Logging - For generating information about the program execution to the user
+# Should be piped to stderr
+# Provide a verbosity flag to allow logging to be turned on or off
+# TODO: Research logging levels
+
+# Data vs. information - JSON or other easily parseable format for output to another program
+# Use of format/color for outputs to the user
+
+# colorama - Library for color
+# Sample for Colorama library
+    # from colorama import Fore, Back, Style
+    # print Fore.RED + 'some red text'
+    # print Back.GREEN + "and with a green background'
+    # print Style.BRIGHT + 'and in bright/bold text',
+    # print Fore.RESET + Back.RESET + Style. RESET_ALL
+    # print 'back to normal now'
+
+# credentials
+# import getpass
+# https://docs.python.org/3/library/getpass.html#module-getpass
+
+# pip install progressbar2
+# import progressbar
+# Features: Progress bar, percent complete, transfer total, transfer speed, ETA
+
